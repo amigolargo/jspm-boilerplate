705598547f189f03c379b913a3097171e8da05b5
Tom Longmate <tomlongmate@gmail.com>
2016-01-03 15:40:02 +0000

    cut the mustard check

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,11 @@
 import _ from 'lodash';
 
-_.times(5, function(i) {
-    console.log('hello: ' + i)
-});
+
+if ('visibilityState' in document) {
+	// https://justmarkup.com/log/2015/02/cut-the-mustard-revisited/
+    console.log('Modern browser. Do some stuff');
+
+    _.times(5, function(i) {
+    	console.log('hello: ' + i)
+	});
+}
